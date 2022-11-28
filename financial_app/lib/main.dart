8b63ed98e9b95ff2fe4db7b682917967ec424b67
Jason <jasonmchenry27@gmail.com>
2022-11-28 18:24:21 -0500

    Firebase changes

--- financial_app/lib/main.dart
+++ financial_app/lib/main.dart
@@ -3,23 +3,21 @@ import 'package:financial_app/ui/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'ui/home_page.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
-      routes: <String, WidgetBuilder> {
-        '/splash':(BuildContext context) => const SplashScreen(),
-        '/login' :(BuildContext context) => const LoginScreen(),
-        '/home':(BuildContext context) => const HomePage(title: 'Home Page'),
+      routes: <String, WidgetBuilder>{
+        '/splash': (BuildContext context) => const SplashScreen(),
+        '/login': (BuildContext context) => const LoginScreen(),
+        '/home': (BuildContext context) => const HomePage(title: 'Home Page'),
       },
     );
   }
