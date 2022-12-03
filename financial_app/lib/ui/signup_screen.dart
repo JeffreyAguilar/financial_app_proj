405d9478aeff36b0c083ad317ff66acf7f6452d2
Jason <jasonmchenry27@gmail.com>
2022-12-03 00:15:36 -0500

    temp

--- financial_app/lib/ui/signup_screen.dart
+++ financial_app/lib/ui/signup_screen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 class SignUpScreen extends StatefulWidget {
   final VoidCallback onClickedSignIn;
