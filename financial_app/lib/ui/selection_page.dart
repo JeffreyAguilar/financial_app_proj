2a480f4ed9fe6e88776b5daff3bbd2e30c9d5044
Jeffrey Aguilar <hostilezzreturn@gmail.com>
2022-12-07 00:50:07 -0500

    Selection Page UI

--- financial_app/lib/ui/selection_page.dart
+++ financial_app/lib/ui/selection_page.dart
@@ -1,17 +1,15 @@
-import 'package:financial_app/ui/home_page.dart';
-import 'package:financial_app/ui/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:financial_app/ui/child_homepage.dart';
+import 'package:financial_app/ui/home_page.dart';
 
-import 'child_homepage.dart';
-
-class selectionPage extends StatefulWidget {
-  const selectionPage({super.key});
+class SelectionPage extends StatefulWidget {
+  const SelectionPage({super.key});
 
   @override
-  State<selectionPage> createState() => _selectionPageState();
+  State<SelectionPage> createState() => _SelectionPageState();
 }
 
-class _selectionPageState extends State<selectionPage> {
+class _SelectionPageState extends State<SelectionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,43 +24,64 @@ class _selectionPageState extends State<selectionPage> {
             ],
           ),
         ),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            textStyle: const TextStyle(fontSize: 20.0),
-          ),
-          onPressed: (() {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return const HomePage();
-            }));
-          }),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 10.0,
-              horizontal: 50.0,
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const HomePage();
+                        },
+                      ),
+                    );
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 50.0,
+                    ),
+                    child: Text('Sign In As Parent'),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const ChildHomePage();
+                        },
+                      ),
+                    );
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 50.0,
+                    ),
+                    child: Text('Sign In As Child'),
+                  ),
+                ),
+              ],
             ),
-            child: Text('Sign In As Parent'),
-          ),
-        ),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            textStyle: const TextStyle(fontSize: 20.0),
-          ),
-          onPressed: (() {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return const ChildHomePage();
-            }));
-          }),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 10.0,
-              horizontal: 50.0,
-            ),
-            child: Text('Sign In As Parent'),
           ),
         ),
       ),
     );
   }
 }
-
-class FirebaseAuth {}
